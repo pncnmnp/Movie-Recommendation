63590030cea2ad69e38eacd1f6e8e9b34db80db9
parthparikh <parthparikh1999p@gmail.com>
2019-09-25 22:52:36 +0530

    Added database for movie posters

--- file_paths.py
+++ file_paths.py
@@ -5,6 +5,7 @@ PATH_RATINGS_FULL = "./movies_meta/ratings.csv"
 PATH_INDIAN_MOVIES = "./movies_meta/indian_movies.csv"
 PATH_CREDITS = "./movies_meta/tmdb_5000_credits.csv"
 PATH_MOVIELENS_TO_TMDB = "./movies_meta/movielens_to_tmdb.csv"
+PATH_POSTERS = "./movies_meta/posters.csv"
 
 # errors
 CATEGORY_ERROR = 'Category not found! Here are the genres: '
